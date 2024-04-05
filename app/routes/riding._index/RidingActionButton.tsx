8c0dec9893f624d281a0c9caf4e53021bea75aa9
IceBing <xu2237803016@outlook.com>
2024-04-05 16:28:31 +0800

    chore: 移除定点骑行功能

--- app/routes/riding._index/RidingActionButton.tsx
+++ app/routes/riding._index/RidingActionButton.tsx
@@ -1,33 +1,26 @@
-import React, { useState } from 'react'
-import { ActionSheet, Button } from 'antd-mobile'
+import type React from 'react'
+import { Button } from 'antd-mobile'
 import { useNavigate } from 'react-router'
 
-const actions = [
-  { text: '自由骑行', key: 'freedom', path: '/riding/freedom' },
-  { text: '定点骑行', key: 'point', path: '/riding/todo' }
-]
+// 定点骑行被砍掉了QAQ
+// const actions = [
+//   { text: '自由骑行', key: 'freedom', path: '/riding/freedom' },
+//   { text: '定点骑行', key: 'point', path: '/riding/located' }
+// ]
 
 interface StartRidingProps {
 }
 
 const RidingActionButton: React.FC<StartRidingProps> = () => {
-  const [visible, setVisible] = useState(false)
   const navigate = useNavigate()
 
-  const onSelect = (_: unknown, index: number) => {
-    setVisible(false)
-    navigate(actions[index].path)
-  }
 
   return (
     <>
       <div style={{ padding: '0 8px' }}>
-        <Button color='primary' block onClick={() => setVisible(true)}>开始骑行</Button>
+        <Button color='primary' block onClick={() => navigate('/riding/freedom')}>开始骑行</Button>
       </div>
-      <ActionSheet onAction={onSelect}
-        actions={actions}
-        visible={visible}
-        onClose={() => setVisible(false)}/>
+
     </>
   )
 }
